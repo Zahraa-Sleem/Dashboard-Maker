8028f7f9321558994cc142268f919eac7c0e5e6e
Zahraa Sleem <zahraasleem03@gmail.com>
2023-12-17 12:00:34 +0200

    Main Page steps

--- DashboardMaker/Controllers/ColorController.cs
+++ DashboardMaker/Controllers/ColorController.cs
@@ -13,21 +13,5 @@ namespace DashboardMaker.Controllers
         {
             _context = context;
         }
-
-        public Color GetColor(string hexadecimal)
-        {
-            //fetch the color from the database
-            var colorInDb = _context.Colors.Find(hexadecimal);
-            if(colorInDb == null)
-            {
-                // Color doesn't exist, so add it to the database
-                var newColor = new Color(hexadecimal);
-                _context.Colors.Add(newColor);
-                _context.SaveChanges();
-            }
-
-            // Return the color object
-            return _context.Colors.Find(hexadecimal);
-        }
     }
 }
---
+++ DashboardMaker/Controllers/api/DashBoardController.cs
@@ -0,0 +1,49 @@
+﻿using DashboardMaker.Data;
+using DashboardMaker.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace DashboardMaker.Controllers.api
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DashboardController : ControllerBase
+	{
+		private readonly ApplicationDbContext _context;
+
+		public DashboardController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet("GetUserDashboards")]
+		public async Task<ActionResult<IEnumerable<Dashboard>>> GetDashboardsForUser()
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			if (string.IsNullOrEmpty(userId))
+			{
+				return Unauthorized();
+			}
+
+			var user = await _context.Users.FindAsync(userId);
+			if (user == null)
+			{
+				return NotFound("User not found.");
+			}
+
+			var dashboards = await _context.Dashboards
+										   .Where(d => d.Owner.Id == user.Id)
+										   .ToListAsync();
+
+			if (!dashboards.Any())
+			{
+				return NotFound("No dashboards found for the user.");
+			}
+
+			return dashboards;
+		}
+
+	}
+}
